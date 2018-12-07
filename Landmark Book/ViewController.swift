96a24727b93f3606c5e82c10178b9df083d7496c
Omer <omer@Omer-MacBook-Pro.local>
2018-12-07 17:52:17 +0300

    Initial Commit

---
+++ Landmark Book/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Landmark Book
+//
+//  Created by Omer  on 7.12.2018.
+//  Copyright © 2018 Omer Varoglu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
